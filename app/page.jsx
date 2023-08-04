5fb7ac00bdcef86f67c49f2dbb7c1e16a922f97c
Antony Jaison <antonyjaison639@gmail.com>
2023-08-04 23:23:59 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,5 @@
+import CommingSoon from "@components/CommingSoon";
+
+export default function Home() {
+  return <CommingSoon/>
+}
---
+++ components/CommingSoon.jsx
@@ -0,0 +1,9 @@
+import styles from '@styles/commingsoon.module.scss'
+
+const CommingSoon = () => {
+  return (
+    <div>CommingSoon</div>
+  )
+}
+
+export default CommingSoon
